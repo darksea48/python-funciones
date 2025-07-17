d28a6d61b9c1c2c33e6b8c95eed7c95c7bab3082
Douglas Suárez Zamorano <d.suarez.zamorano@gmail.com>
2025-07-17 09:50:25 -0400

    -realización ejercicio AE6 / 17-07-2025 9:50

---
+++ AE6-ABP-Ejercicio individual/ejercicio_funciones.py
@@ -0,0 +1,105 @@
+#  1. Actualizar valores en diccionarios y listas
+matriz = [ [10, 15, 20], [3, 7, 14] ]
+
+cantantes = [
+   {"nombre": "Ricky Martin", "pais": "Puerto Rico"},
+   {"nombre": "Chayanne", "pais": "Puerto Rico"}
+]
+
+ciudades = {
+   "México": ["Ciudad de México", "Guadalajara", "Cancún"],
+   "Chile": ["Santiago", "Concepción", "Viña del Mar"]
+}
+
+coordenadas = [
+   {"latitud": 8.2588997, "longitud": -84.9399704}
+]
+
+# Cambia el valor de 3 en matriz por 6.
+def cambio_matriz():
+    for i in range(len(matriz)):
+        for j in range(len(matriz[i])):
+            if matriz[i][j] == 3:
+                matriz[i][j] = 6
+                break
+    print(matriz)
+
+cambio_matriz()
+
+# Cambia el nombre del primer cantante de “Ricky Martin” a “Enrique Martin Morales”
+def cambio_nombre_cantante():
+    for i in range(len(cantantes)):
+        if cantantes[i]["nombre"] == "Ricky Martin":
+            cantantes[i]["nombre"] = "Enrique Martin Morales"
+            break
+    print(cantantes)
+
+cambio_nombre_cantante()
+
+# En ciudades, cambia “Cancún” por “Monterrey”
+def cambio_ciudad():
+    for i in ciudades:
+        for j in range(len(ciudades[i])):
+            if ciudades[i][j] == "Cancún":
+                ciudades[i][j] = "Monterrey"
+                break
+    print(ciudades)
+
+cambio_ciudad()
+
+# En las coordenadas, cambia el valor de “latitud” por 9.9355431
+def cambio_coordenadas(coordenada=9.9355431):
+    for i in range(len(coordenadas)):
+        if coordenadas[i]["latitud"] == 8.2588997:
+            coordenadas[i]["latitud"] = coordenada
+            break
+    print(coordenadas)
+
+cambio_coordenadas()
+
+# 2. Iterar a través de una lista de diccionarios: Crea la función iterarDiccionario(lista) que reciba una lista de diccionarios y recorra cada diccionario de la lista e imprima cada llave y el valor correspondiente.
+cantantes = [
+   {"nombre": "Ricky Martin", "pais": "Puerto Rico"},
+   {"nombre": "Chayanne", "pais": "Puerto Rico"},
+   {"nombre": "José José", "pais": "México"},
+   {"nombre": "Juan Luis Guerra", "pais": "República Dominicana"}
+]
+
+def iterarDiccionario(lista):
+    for i in lista:
+        print(f"Nombre - {i['nombre']}, País - {i['pais']}")
+
+iterarDiccionario(cantantes)
+print()
+# 3. Obtener valores de una lista de diccionarios Crea la función iterarDiccionario2(llave, lista) que reciba una cadena con el nombre de una llave y una lista de diccionarios. La función debe imprimir el valor almacenado para esa clave de cada diccionario que se encuentra en la lista
+def iterarDiccionario2(llave, lista):
+    for i in lista:
+        print(i[llave])
+
+iterarDiccionario2("nombre", cantantes)
+print()
+iterarDiccionario2("pais", cantantes)
+print()
+
+# 4. Iterar a través de un diccionario con valores de lista Crea una función imprimirInformacion(diccionario) que reciba un diccionario en donde los valores son listas. La función debe imprimir el nombre de cada clave junto con el tamaño de su lista y seguido de esto los valores de la lista para esa clave.
+costa_rica = {
+   "ciudades": ["San José", "Limón", "Cartago", "Puntarenas"],
+   "comidas": ["gallo pinto", "casado", "tamales", "chifrijo", "olla de carne"]
+}
+chile = {
+   "ciudades": ["Santiago", "Concepción", "Viña del Mar", "Punta Arenas"],
+   "comidas": ["empanada de pino", "cazuela", "curanto", "mote con huesillos", "terremoto"]
+}
+
+def imprimirInformacion(diccionario):
+    for clave, lista_valores in diccionario.items():
+        print(f"{len(lista_valores)} {clave.upper()}")
+        for valor in lista_valores:
+            if clave == "ciudades":
+                print(valor)
+            else:
+                print(valor.capitalize())
+        print()
+
+imprimirInformacion(costa_rica)
+imprimirInformacion(chile)
